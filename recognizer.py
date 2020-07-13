1984ac28db0699d0bf3506982553f9444e467dfc
RitaRez <ritarezborges@gmail.com>
2020-07-13 20:57:46 -0300

    data preprocessing is done

---
+++ recognizer.py
@@ -0,0 +1,51 @@
+import pandas as pd
+import matplotlib.pyplot as plt
+import numpy as np
+import tensorflow as tf
+
+from sklearn.model_selection import train_test_split
+from sklearn.preprocessing import StandardScaler
+from sklearn.metrics import accuracy_score
+
+def data_preprocessing(y):
+    new_y = []
+    
+    for el in y:
+        arr = [0] * 26
+        arr[el - 1] = 1
+        new_y.append(arr)
+    
+    return new_y
+
+def feature_scaling(data):
+    sc = StandardScaler()
+    data = sc.fit_transform(data)
+
+    return data
+
+def neural_network(x_train, y_train):
+    
+    nn = tf.keras.models.Sequential([
+        tf.keras.layers.Flatten(),
+        tf.keras.layers.Dense(units=128, activation='relu'),
+        tf.keras.layers.Dense(units=26, activation='sigmoid')
+    ])
+
+    nn.compile(optimizer = 'adam', loss = 'binary_crossentropy', metrics = ['accuracy'])
+    nn.fit(x_train, y_train,  epochs=900)  
+
+    return nn
+    
+def main():
+    data = pd.read_csv('./MNIST/sign_mnist_train.csv')
+    x = data.iloc[:, 1:-1]
+    y = data_preprocessing(data.iloc[:, 0])
+
+    x_train, x_test,y_train, y_test = train_test_split(x,y, test_size = 0.2, random_state = 1)
+
+    x_train = feature_scaling(x_train)
+    x_test = feature_scaling(x_test)
+    
+    #nn = neural_network(x_train, y_train)
+
+main()
