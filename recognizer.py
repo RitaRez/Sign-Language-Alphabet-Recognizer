2f49614b634d0036afb7e98732c5b35675009ef9
RitaRez <ritarezborges@gmail.com>
2020-07-14 11:25:39 -0300

    webcam reader is working

--- recognizer.py
+++ recognizer.py
@@ -6,6 +6,9 @@ import tensorflow as tf
 from sklearn.model_selection import train_test_split
 from sklearn.preprocessing import StandardScaler
 from sklearn.metrics import accuracy_score
+from tensorflow.keras.layers import Dense, Flatten, Conv2D
+from tensorflow.keras import Model
+
 
 def data_preprocessing(y):
     new_y = []
@@ -23,6 +26,20 @@ def feature_scaling(data):
 
     return data
 
+class MyModel(Model):
+  def __init__(self):
+    super(MyModel, self).__init__()
+    self.conv1 = Conv2D(32, 3, activation='relu')
+    self.flatten = Flatten()
+    self.d1 = Dense(128, activation='relu')
+    self.d2 = Dense(10)
+
+  def call(self, x):
+    x = self.conv1(x)
+    x = self.flatten(x)
+    x = self.d1(x)
+    return self.d2(x)
+
 def neural_network(x_train, y_train):
     
     nn = tf.keras.models.Sequential([
@@ -32,7 +49,7 @@ def neural_network(x_train, y_train):
     ])
 
     nn.compile(optimizer = 'adam', loss = 'binary_crossentropy', metrics = ['accuracy'])
-    nn.fit(x_train, y_train,  epochs=900)  
+    nn.fit(x_train, y_train,  epochs=3)  
 
     return nn
     
@@ -41,11 +58,10 @@ def main():
     x = data.iloc[:, 1:-1]
     y = data_preprocessing(data.iloc[:, 0])
 
-    x_train, x_test,y_train, y_test = train_test_split(x,y, test_size = 0.2, random_state = 1)
+    x_train, x_test, y_train, y_test = train_test_split(x,y, test_size = 0.2, random_state = 1)
 
     x_train = feature_scaling(x_train)
     x_test = feature_scaling(x_test)
     
-    #nn = neural_network(x_train, y_train)
+    nn = neural_network(x_train, y_train)
 
-main()
---
+++ webcam_handler.py
@@ -0,0 +1,34 @@
+import cv2 as cv
+import matplotlib.pyplot as plt
+import matplotlib.image as mpimg
+import numpy as np
+import os
+from skimage import io
+
+os.environ['DISPLAY'] = ':0'
+
+def get_image_from_webcam():
+    cap = cv.VideoCapture(0)
+    
+    if not cap.isOpened():
+        print("Cannot open camera")
+        exit()
+    while True:
+        ret, frame = cap.read()
+        if not ret:
+            print("Can't receive frame (stream end?). Exiting ...")
+            break
+        
+        order = input()
+        if (order == '\n') or (order == ' ') or cv.waitKey(1) == ord('q'):
+            print('Taking picture.')
+            cap.release()
+            cv.destroyAllWindows()
+            io.imsave('dataset/webcam_pictures.jpeg', frame)
+            return frame
+    
+    
+    
+
+
+frame = get_image_from_webcam()
