7460acac81e059989ee43ac1c9d3239abe2daad2
RitaRez <ritarezborges@gmail.com>
2020-07-15 23:01:33 -0300

    image resizing is working

--- Videorecognition/src/webstreaming.py
+++ Videorecognition/src/webstreaming.py
@@ -1,5 +1,6 @@
-import os
+import os, sys
 import numpy as np
+import matplotlib.pyplot as plt
 
 from importlib import import_module 
 from flask import Flask, render_template, Response, render_template_string
@@ -7,9 +8,11 @@ from camera import Camera
 from skimage import io, color
 from recognizer import NeuralNetwork
 from random import random
+from PIL import Image
 
 app = Flask(__name__)
 cam = Camera()
+BOX_SIZE = 180
 
 @app.route("/")
 def index():
@@ -36,14 +39,24 @@ def predict_answer():
 
     pic_name = '../../dataset/pic'+pic_number+'.jpg'
     gray_pic_name = '../../dataset/gray'+pic_number+'.jpg'
+    gray_croped_name = '../../dataset/gray_croped'+pic_number+'.jpg'
 
-    io.imsave(pic_name, current_frame)
+    #io.imsave(pic_name, current_frame)
     io.imsave(gray_pic_name, gray_frame)
 
-    #model = NeuralNetwork()
-    print('\n\n\n', gray_frame.shape)
-    # model.predict_pic(current_frame)
-    prediction = 'Letter Shown: A'
+    gray_frame = Image.open(gray_pic_name)
+    gray_frame_crop = gray_frame.crop((80,225,80+BOX_SIZE,225+BOX_SIZE))
+
+    
+    gray_frame_crop.thumbnail((28, 28), Image.ANTIALIAS)
+    gray_frame_crop.save(gray_croped_name)
+            
+    pix = np.array(gray_frame_crop.getdata()).reshape(1, gray_frame_crop.size[0], gray_frame_crop.size[1], 1)
+    print(pix.shape)
+    
+    model = NeuralNetwork()
+    prediction = model.predict_pic(pix)
+    prediction = 'Letter Shown: ' + prediction
     return prediction
 
 
